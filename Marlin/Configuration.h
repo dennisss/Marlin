c0b1bc7a4281c67bb3fb20480ab360f417a06b18
Dennis Shtatnov <densht@gmail.com>
2017-05-29 11:29:50 -0400

    Calibrate probe offset

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -628,7 +628,7 @@
 //  (0,0)
 #define X_PROBE_OFFSET_FROM_EXTRUDER 10  // X offset: -left  +right  [of the nozzle]
 #define Y_PROBE_OFFSET_FROM_EXTRUDER 10  // Y offset: -front +behind [the nozzle]
-#define Z_PROBE_OFFSET_FROM_EXTRUDER 0   // Z offset: -below +above  [the nozzle]
+#define Z_PROBE_OFFSET_FROM_EXTRUDER -0.9   // Z offset: -below +above  [the nozzle]
 
 // X and Y axis travel speed (mm/m) between probes
 #define XY_PROBE_SPEED 8000
@@ -916,7 +916,7 @@
 // For DELTA this is the top-center of the Cartesian print volume.
 #define MANUAL_X_HOME_POS 0
 #define MANUAL_Y_HOME_POS 0
-#define MANUAL_Z_HOME_POS 284.7 // Distance between the nozzle to printbed after homing
+#define MANUAL_Z_HOME_POS 281.7 // Distance between the nozzle to printbed after homing
 
 // Use "Z Safe Homing" to avoid homing with a Z probe outside the bed area.
 //
